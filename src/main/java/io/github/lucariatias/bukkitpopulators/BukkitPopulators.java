7d6ba87ad0a99ae8fbafa1fbe786b30626a1ba2b
Lucariatias <lucariatias@gmail.com>
2013-09-13 23:57:09 +0100

    Add ore populator

--- src/main/java/io/github/lucariatias/bukkitpopulators/BukkitPopulators.java
+++ src/main/java/io/github/lucariatias/bukkitpopulators/BukkitPopulators.java
@@ -3,6 +3,7 @@ package io.github.lucariatias.bukkitpopulators;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
@@ -19,48 +20,54 @@ import uk.co.jacekk.bukkit.skylandsplus.generation.SnowPopulator;
 
 public class BukkitPopulators extends JavaPlugin {
 
-
 	@Override
 	public void onEnable() {
 		PluginManager pluginManager = getServer().getPluginManager();
 		Plugin skylandsPlus = pluginManager.getPlugin("SkylandsPlus");
-		if(skylandsPlus != null) {
+		if (skylandsPlus != null) {
 			this.getLogger().info("Loading populators from SkylandsPlus");
 		}
 	}
 
 	/**
 	 * Gets the "normal" populators for a particular world.
-	 *
-	 * This function works like the ChunkGenerator function, but is not
-	 * called by bukkit directly.
+	 * 
+	 * This function works like the ChunkGenerator function, but is not called
+	 * by bukkit directly.
+	 * 
 	 * @param world the world
 	 * @return the normal populators for a world
 	 * @see ChunkGenerator#getDefaultPopulators(World)
 	 */
-	public List<BlockPopulator> getDefaultPopulators(World world){
+	public List<BlockPopulator> getDefaultPopulators(World world) {
 		ArrayList<BlockPopulator> populators = new ArrayList<BlockPopulator>();
 
 		PluginManager pluginManager = this.getServer().getPluginManager();
 		Plugin skylandsPlus = pluginManager.getPlugin("SkylandsPlus");
 
-		switch (world.getEnvironment()){
+		switch (world.getEnvironment()) {
 			case NORMAL:
 				populators.add(new BiomeTreePopulator());
-				if(skylandsPlus != null) {
+				populators.add(new OrePopulator(Material.COAL_ORE, Material.STONE, 8, 8));
+				populators.add(new OrePopulator(Material.IRON_ORE, Material.STONE, 6, 6));
+				populators.add(new OrePopulator(Material.LAPIS_ORE, Material.STONE, 2, 4));
+				populators.add(new OrePopulator(Material.GOLD_ORE, Material.STONE, 4, 4));
+				populators.add(new OrePopulator(Material.DIAMOND_ORE, Material.STONE, 3, 4));
+				populators.add(new OrePopulator(Material.REDSTONE_ORE, Material.STONE, 4, 16));
+				populators.add(new OrePopulator(Material.EMERALD_ORE, Material.STONE, 1, 3));
+				if (skylandsPlus != null) {
 					populators.add(new BiomePopulator());
 					populators.add(new SnowPopulator());
 				}
 				break;
-	
 			case THE_END:
-				if(skylandsPlus != null) {
+				if (skylandsPlus != null) {
 					populators.add(new EndTowerPopulator(world));
 				}
 				break;
-	
 			case NETHER:
-				if(skylandsPlus != null) {
+				populators.add(new OrePopulator(Material.QUARTZ_ORE, Material.NETHERRACK, 16, 8));
+				if (skylandsPlus != null) {
 					populators.add(new NetherSoulSandPopulator(world));
 					populators.add(new NetherFirePopulator(world));
 					populators.add(new NetherGlowstonePopulator(world));
@@ -73,20 +80,29 @@ public class BukkitPopulators extends JavaPlugin {
 
 	/**
 	 * Get all populators available
-	 *
+	 * 
 	 * @param world the world
 	 * @return all the populators available for the world
 	 */
 	public List<BlockPopulator> getAllPopulators(World world) {
 		ArrayList<BlockPopulator> populators = new ArrayList<BlockPopulator>();
-		
+
 		populators.add(new BiomeTreePopulator());
+		populators.add(new BiomeTreePopulator());
+		populators.add(new OrePopulator(Material.COAL_ORE, Material.STONE, 8, 8));
+		populators.add(new OrePopulator(Material.IRON_ORE, Material.STONE, 6, 6));
+		populators.add(new OrePopulator(Material.LAPIS_ORE, Material.STONE, 2, 4));
+		populators.add(new OrePopulator(Material.GOLD_ORE, Material.STONE, 4, 4));
+		populators.add(new OrePopulator(Material.DIAMOND_ORE, Material.STONE, 3, 4));
+		populators.add(new OrePopulator(Material.REDSTONE_ORE, Material.STONE, 4, 16));
+		populators.add(new OrePopulator(Material.EMERALD_ORE, Material.STONE, 1, 3));
+		populators.add(new OrePopulator(Material.QUARTZ_ORE, Material.NETHERRACK, 16, 8));
 
 		PluginManager pluginManager = getServer().getPluginManager();
-		
-		//SkylandsPlus
+
+		// SkylandsPlus
 		Plugin skylandsPlus = pluginManager.getPlugin("SkylandsPlus");
-		if(skylandsPlus != null) {
+		if (skylandsPlus != null) {
 			populators.add(new BiomePopulator());
 			populators.add(new SnowPopulator());
 			populators.add(new EndTowerPopulator(world));
